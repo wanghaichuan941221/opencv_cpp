e7049dcd4eeb8b50da1f4c938739460aa72594b9
wanghaichuan941221 <wanghaichuan941221@gmail.com>
2019-07-12 16:42:22 +0800

    remove frangi

--- frangi/frangi.h
+++ frangi/frangi.h
@@ -1,14 +1,5 @@
-﻿#include<opencv2/opencv.hpp>
-#include <cmath>
-
-#define PI 3.1415926
-
-// //定义导出函数
-// #if (defined WIN32 || defined _WIN32 || defined WINCE) && defined FRANGILIB_EXPORTS
-// # define FRANGI_EXPORTS __declspec(dllexport)
-// #else
-// # define FRANGI_EXPORTS
-// #endif
+﻿#include <cmath>
+#include <opencv2/opencv.hpp>
 
 class FrangiFilter {
 private:
@@ -16,8 +7,6 @@ private:
 	float beta1, beta2;
 	bool black_ridge;
 public:
-	//FrangiFilter();
-	
 	virtual void processImage(const cv::Mat &src,cv::Mat &dst)=0;
 	virtual void setSigma_start(float sigma_start_)=0;
 	virtual void setSigma_end(float sigma_end_)=0;
@@ -28,7 +17,6 @@ public:
 
 };
 
-//导出createFrangiFilter函数
 cv::Ptr<FrangiFilter> createFrangiFilter(float sigma_start_=1.0, float sigma_end_=10.0, float sigma_step_=2.0, float beta1_=0.5, float beta2_=15.0, bool black_ridge_=true);
 
 
@@ -38,7 +26,6 @@ private:
 	float beta1, beta2;
 	bool black_ridge;
 
-	//void Hessian2D(const cv::Mat &src, cv::Mat Dxx, cv::Mat Dxy, cv::Mat Dyy, float sigma);
 	void Eig2Image(cv::Mat Dxx, cv::Mat Dxy, cv::Mat Dyy, cv::Mat &lambda1, cv::Mat &lambda2);
 	void Gradient_x(const cv::Mat &src, cv::Mat &Dx);
 	void Gradient_y(const cv::Mat &src, cv::Mat &Dy);
